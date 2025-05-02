1e1a95db3bb39ac168e9a1ef45bf10d92ada53d8
Oscar Topliss <oscartopliss@gmail.com>
2025-05-02 17:18:50 +0100

    Added self-signed-cert logic to server program

---
+++ server.py
@@ -0,0 +1,61 @@
+######################### SERVER PROGRAM #######################################
+# The server program, which contains most of the functionality of the system.
+
+## Imports
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography import x509
+from cryptography.x509 import NameOID, Certificate
+import datetime
+
+##### PKI
+# Generates a new X.509 self-signed certificate, which will be used for TLS
+# encryption. Stores the private key in an "HSM". The certificate is stored in
+# ./shared-certificates, which is accessible to the client program.
+# Mostly followed the tutorial at https://cryptography.io/en/latest/x509/tutorial/#creating-a-self-signed-certificate
+def gen_self_signed_cert():
+    key = ec.generate_private_key(ec.SECP521R1())
+    public_key = key.public_key()
+
+    with open("./HSM-server/private-key.pem", "wb") as keyfile:
+        keyfile.write(key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.\
+            BestAvailableEncryption(b"super-secure-passphrase"),
+        ))
+
+    subject = issuer = x509.Name([
+            x509.NameAttribute(NameOID.COUNTRY_NAME, "UK"),
+            x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "West Midlands"),
+            x509.NameAttribute(NameOID.LOCALITY_NAME, "Coventry"),
+            x509.NameAttribute(NameOID.ORGANIZATION_NAME, "Definitely Real CA Inc."),
+            x509.NameAttribute(NameOID.COMMON_NAME, "Definitely Real CA Inc. Root CA"),
+        ])
+
+    serial_number = x509.random_serial_number()
+
+    current_date = datetime.datetime.now(datetime.timezone.utc)
+
+    expiry_date = datetime.datetime.now(datetime.timezone.utc) + \
+    datetime.timedelta(days=365 * 10)
+
+    cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        public_key
+    ).serial_number(
+        serial_number
+    ).not_valid_before(
+        current_date
+    ).not_valid_after(
+        expiry_date
+    ).add_extension(
+        x509.SubjectAlternativeName([x509.DNSName("localhost")]),
+        critical=False
+    ).sign(key, hashes.SHA512())
+
+    with open("./shared-certiciates", "wb") as certfile:
+        certfile.write(cert.public_bytes(serialization.Encoding.PEM))
