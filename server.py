a2adfaad1cadd68dc8258cca1305b93cca801f48
Oscar Topliss <oscartopliss@gmail.com>
2025-05-05 15:20:24 +0100

    Further developed server logic

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 
 ## Imports
 # Cryptography
+from errno import EWOULDBLOCK
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography import x509
@@ -11,6 +12,8 @@ import datetime
 # Sockets/SSL
 import socket
 import ssl
+from tkinter.filedialog import Open
+from enum import Enum
 
 ##### PKI
 # Generates a new X.509 self-signed certificate, which will be used for TLS
@@ -63,13 +66,55 @@ def gen_self_signed_cert():
     with open("./shared-certificates/root-certificate.pem", "wb") as certfile:
         certfile.write(cert.public_bytes(serialization.Encoding.PEM))
 
+
+# Class to handle client sessions.
+# Once a client socket is created, it's passed to an instance of this class.
+# Each instance is repsponsible for closing its given clientSocket object, and
+# for returning so that
 class ClientSession:
-    clientSocket = None
     username = None
     userType = None
-    authenticated = False
-    def __init__(self, clientSocket):
+    sessionActive = False
+    def __init__(self, clientSocket: socket.socket):
         self.clientSocket = clientSocket
+        self.clientSocket.setblocking(False)
+        self.sessionActive = True
+
+    # Enum to define message codes, used in the recv_message() function.
+    class MessageCode(Enum):
+        OPEN = 1
+        CLOSED = 2
+        ERROR = 3
+    # Loop which receives a full message. If it receives b'', i.e. the
+    # connection is closed, returns (message, MessageCode), with messageCode
+    # telling the handler whether the connection has been closed or not.
+    def recv_message(self) -> tuple[bytes, MessageCode]:
+        message = b''
+        while True:
+            try:
+                message += self.clientSocket.recv(1024)
+            # Logic to see if there's an actual error, or if it's throwing an
+            # exception because the socket is in non-blocking mode.
+            except socket.error as error:
+                if error.errno != EWOULDBLOCK:
+                    return (message, self.MessageCode.ERROR)
+                if message == b'':
+                    return (message, self.MessageCode.CLOSED)
+                return (message, self.MessageCode.OPEN)
+            except:
+                return (message, self.MessageCode.ERROR)
+
+
+    # Loop which handles a session until it terminates. Returns 0 if the
+    # session ended as espected, 1 if an error occured.
+    def sessionHandlerLoop(self) -> int:
+        while True:
+            message, connectionStatus = self.recv_message()
+
+
+
+
+
 
 
 class Server:
